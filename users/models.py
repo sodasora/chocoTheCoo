68f3bacda600709ab737ebdf2fb29d4bb927a95d
Kim Gwang woon <misili304@naver.com>
2023-06-06 17:43:51 +0900

    :sparkles:Feat: Create cartitem models #3

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from config.models import CommonModel
+from products.models import Product
 
 # Create your models here.
+
+class Cart(CommonModel):
+    """ 장바구니 """
+    
+    pass
+
+
+class CartItem(CommonModel):
+    """ Cart와 Product의 ManyToManyField """
+    product = models.ForeignKey(Product, models.CASCADE, verbose_name="상품", )
+    cart = models.ForeignKey(Cart, models.CASCADE, verbose_name="장바구니", )
+    count = models.PositiveIntegerField("상품개수", default=1)
+    
+    
