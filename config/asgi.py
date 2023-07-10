59839e23c4a83c76cd21f8cee9885502d969ccc0
Kim Gwang woon <misili304@naver.com>
2023-07-10 17:51:46 +0900

    :books:Docs:update README.md

--- config/asgi.py
+++ config/asgi.py
@@ -6,22 +6,23 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 from .middleware import JwtAuthMiddlewareStack
-import os
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 django.setup()
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': JwtAuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    )
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": JwtAuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns)
+        ),
+    }
+)
